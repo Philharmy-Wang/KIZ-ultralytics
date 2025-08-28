bea51cb45a2ee8f50dd05af42fec33ee1265d504
Philharmy-Wang <1271578900@qq.com>
2025-08-28 05:39:57 -0400

    add

--- train_all.sh
+++ train_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-yolo detect train data=fire-smoke.yaml model=yolov5nu.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolo5n
-yolo detect train data=fire-smoke.yaml model=yolov5su.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolo5s
-yolo detect train data=fire-smoke.yaml model=yolo5mu.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolo5m
+# yolo detect train data=fire-smoke.yaml model=yolov5nu.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolo5n
+# yolo detect train data=fire-smoke.yaml model=yolov5su.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolo5s
+yolo detect train data=fire-smoke.yaml model=yolov5mu.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolo5m
 
 yolo detect train data=fire-smoke.yaml model=yolov8n.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolov8n
 yolo detect train data=fire-smoke.yaml model=yolov8s.pt  epochs=100  batch=32 imgsz=640 device=0,1 project=project/fire_smoke name=yolov8s
